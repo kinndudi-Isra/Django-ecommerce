f11805c33f4be4849eeb6a5d8c425c6bbf3b0def
NIYIBIZI HIRWA <61378957+hirwa1@users.noreply.github.com>
2021-06-18 14:44:02 +0200

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+# 
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'z+ksf@)0d^qojbh4rnp4b1to$hq&*tt(3bs$gf(3i267g$k9ln'
